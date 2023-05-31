1dc7256b3ee244c65476dd5e4b9d350ae9484713
Federico Diaz <1410288+fddiaz@users.noreply.github.com>
2023-05-31 16:49:37 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function showAlert(nombre) {
+    let texto = nombre;
+    alert(`¡Hola ${texto} esta es una alerta desde JavaScript!`);
+}
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+const form = document.getElementById('myForm');
+if (form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        validateForm();
+    });
+};
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+        document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+        }
+    )
---
+++ readme.md
@@ -0,0 +1,14 @@
+1 Home
+2 Busqueda
+3 Recetario
+    1 Saludable
+    2 Vegetariano
+    3 Regionales
+    4 Fast food
+4 Favoritas
+5 Mis recetas <- login ? register
+    1 Perfil  
+    2 Listado de recetas <- logueado
+    3 Mis favoritas
+    4 - Cerrar sesión
+6 Contacto/Nosotros
